fb3513f3b957f15f30da0316112af7987c3ff323
Ravshanbek eshquvvatov <ravshanbekeshquvvatov@gmail.com>
2022-06-30 23:37:34 +0500

    fixed

--- lib/pages/main_page/view.dart
+++ lib/pages/main_page/view.dart
@@ -15,62 +15,64 @@ class Main_pagePage extends StatelessWidget {
   Widget _buildPage(BuildContext context) {
     final model = context.watch<Main_pageProvider>();
 
-     if(!model.isLoading) {
-       return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Contacts"),
-        actions: [
-          Icon(Icons.search),
-        ],
-      ),
-      body: Row(
-        children: [
-          Expanded(child:ListView.builder(
-              itemCount:model.users!.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.transparent,
-                    child: CachedNetworkImage(
-                      imageUrl: "",
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) =>
-                          CircleAvatar(
-                            radius: 20,
-                            backgroundColor: Colors.blue.shade200,
-                          ),
-                      errorWidget: (context, url, error) =>
-                          CircleAvatar(
-                            radius: 20,
-                            backgroundColor: Colors.blue.shade200,
-                          ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  title: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(model.users![index].title,style: TextStyle(color: Colors.red),),
-                      Text(model.users![index].body)
-                    ],
-                  ),
-                );
-              })) ,
-        ],
-      ),
-      drawer: const Drawer(),
-    ) ;
-     }
-      return Container(
-        color: Colors.white,
-        child: const Center(
-          child:  CircularProgressIndicator(),
+    if (!model.isLoading) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Contacts"),
+          actions: const [
+            Icon(Icons.search),
+          ],
         ),
+        body: Row(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: model.users!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.transparent,
+                          child: CachedNetworkImage(
+                            imageUrl: "",
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    CircleAvatar(
+                              radius: 20,
+                              backgroundColor: Colors.blue.shade200,
+                            ),
+                            errorWidget: (context, url, error) => CircleAvatar(
+                              radius: 20,
+                              backgroundColor: Colors.blue.shade200,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        title: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              model.users![index].title,
+                              style: const TextStyle(color: Colors.red),
+                            ),
+                            Text(model.users![index].body)
+                          ],
+                        ),
+                      );
+                    })),
+          ],
+        ),
+        drawer: const Drawer(),
       );
+    }
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
-
